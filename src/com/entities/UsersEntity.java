e83d5d7f54600c3b1d8ceea958c5b90407f4520e
Sandrine Schutt <sandrine.schutt@gmail.com>
2018-03-29 15:31:48 +0200

    userEntity

---
+++ src/com/entities/UsersEntity.java
@@ -0,0 +1,63 @@
+package com.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users", schema = "griseau", catalog = "")
+public class UsersEntity {
+    private Integer userId;
+    private String userName;
+    private String userEmail;
+
+    @Id
+    @Column(name = "user_id", nullable = false)
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    @Basic
+    @Column(name = "user_name", nullable = false, length = 30)
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Basic
+    @Column(name = "user_email", nullable = false, length = 30)
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UsersEntity that = (UsersEntity) o;
+
+        if (userId != null ? !userId.equals(that.userId) : that.userId != null) return false;
+        if (userName != null ? !userName.equals(that.userName) : that.userName != null) return false;
+        if (userEmail != null ? !userEmail.equals(that.userEmail) : that.userEmail != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId != null ? userId.hashCode() : 0;
+        result = 31 * result + (userName != null ? userName.hashCode() : 0);
+        result = 31 * result + (userEmail != null ? userEmail.hashCode() : 0);
+        return result;
+    }
+}
